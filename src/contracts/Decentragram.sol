153cbe16e46f7f16e990c500a9a43eb1710acc44
Farzad Sunavala <40604067+farzad528@users.noreply.github.com>
2021-05-14 17:53:53 -0500

    first test and struct Image

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,20 @@
 pragma solidity ^0.5.0;
 
 contract Decentragram {
-  string public name = "Decentragram";
+    string public name = "Decentragram";
+
+    // Store Images
+    mapping(uint => Image) public images;
+
+    struct Image {
+      uint id;
+      string hash;
+      string description; 
+      uint tipAmount;
+      address payable author;
+    }
+
+    // Create Images
+
+    // Tip Images
 }
